754304f4c0f89a510c2db5ca2aee2e1995f1280c
Rashed <rashedulhoqueceo@gmail.com>
2024-04-20 22:23:02 +0600

    creat user first post

--- src/Components/UserPost.jsx
+++ src/Components/UserPost.jsx
@@ -1,10 +1,69 @@
 import { Flex } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { Avatar } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
+import { Image } from '@chakra-ui/react';
+import { BsThreeDots } from 'react-icons/bs';
 
 const UserPost = () => {
     return (
-        <Flex>
-            
-        </Flex>
+        <Link to="/mark/post/1">
+            <Flex gap={3} mb={4} py={5}>
+                <Flex flexDirection={'column'} alignItems={'center'}>
+                    <Avatar size={'md'} name='markzuckerbarg' src='/public/zuck-avatar.png' />
+                    <Box w={1} h={'full'} bg={'gray.light'} my={2}></Box>
+                    <Box position={'relative'} w={'full'}>
+                        <Avatar
+                            size={'xs'}
+                            name='Dan Abramov'
+                            src='https://bit.ly/dan-abramov'
+                            position={'absolute'}
+                            top={'0px'}
+                            left={'15px'}
+                            padding={'2px'}
+                        />
+                        <Avatar
+                            size={'xs'}
+                            name='Kent Dodds'
+                            src='https://bit.ly/kent-c-dodds'
+                            position={'absolute'}
+                            bottom={'0px'}
+                            right={'-5px'}
+                            padding={'2px'}
+                        />
+                        <Avatar
+                            size={'xs'}
+                            name='Ryan Florence'
+                            src='https://bit.ly/ryan-florence'
+                            position={'absolute'}
+                            bottom={'0px'}
+                            left={'4px'}
+                            padding={'2px'}
+                        />
+                    </Box>
+                </Flex>
+                <Flex flex={1} flexDirection={'column'} gap={2}>
+                    <Flex justifyContent={'space-between'} w={'full'}>
+                        <Flex w={'full'} alignItems={'center'}>
+                            <Text fontSize={'sm'} fontWeight={'bold'}>
+                                markzuckerberg
+                            </Text>
+                            <Image src='/public/verified.png' w={4} h={4} ml={1} />
+                        </Flex>
+                        <Flex gap={4} alignItems={'center'}>
+                            <Text fontStyle={'sm'} color={'gray.light'}>1d</Text>
+                            <BsThreeDots />
+                        </Flex>
+                    </Flex>
+
+                    <Text fontSize={'sm'}>This is my first post.</Text>
+<Box borderRadius={6} overflow={'hidden'} border={'1px solid'} borderColor={'gray.light'}> 
+<Image src='/public/post1.png' w={'full'} />
+</Box>
+                </Flex>
+            </Flex>
+        </Link>
     );
 };
 
